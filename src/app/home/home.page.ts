dd119ffce8b9f2894a49b0df4a01cde0f8b79479
Luis Jair <naopubg28@gmail.com>
2025-05-12 15:37:34 -0600

    create login page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.css'],
   standalone: false,
 })
 export class HomePage {
