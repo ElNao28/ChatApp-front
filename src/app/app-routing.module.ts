44b2075e64687a8416badd2dbba8fae3706671ad
luis jair <jair.escalante@phs.mx>
2025-05-12 16:51:03 -0600

    create register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'login',
     pathMatch: 'full',
+  },  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
+
 ];
 
 @NgModule({
