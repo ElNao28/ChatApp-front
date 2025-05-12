b86082cd20f877885ca6cf8c1cd3c9f660d08fb7
Luis Jair <naopubg28@gmail.com>
2025-05-12 13:59:04 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test-css',
+  webDir: 'www'
+};
+
+export default config;
