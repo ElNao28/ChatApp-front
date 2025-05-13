0eb622c34a49e2a9f71f29c0356cff731af95ae6
luis jair <jair.escalante@phs.mx>
2025-05-13 12:19:52 -0600

    styles to list user and filter users

--- src/app/pages/list-users/list-users.page.ts
+++ src/app/pages/list-users/list-users.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { User } from 'src/app/interfaces/user.interface';
 
 @Component({
@@ -10,8 +11,18 @@ import { User } from 'src/app/interfaces/user.interface';
 })
 export class ListUsersPage implements OnInit {
   constructor(private activateRouter: ActivatedRoute) {}
+  private jwtHelper = new JwtHelperService();
   public users: User[] = [];
   ngOnInit() {
-    this.activateRouter.data.subscribe(({ users }) => (this.users = users));
+    this.activateRouter.data.subscribe(({ users }) => {
+      this.users = users;
+      const user = this.decodeToken();
+      this.users = this.users.filter((u) => u.id !== user.id);
+    });
+  }
+  private decodeToken(): User {
+    const token = localStorage.getItem('token');
+    const decodedToken: User = this.jwtHelper.decodeToken(token!)!;
+    return decodedToken;
   }
 }
