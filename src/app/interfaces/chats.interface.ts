78ae15109fc837c7d4a0ec158b478db7c428f159
luis jair <jair.escalante@phs.mx>
2025-05-13 01:37:58 -0600

    redesing home page

--- src/app/interfaces/chats.interface.ts
+++ src/app/interfaces/chats.interface.ts
@@ -1,9 +1,16 @@
 export interface Chats {
   id: string;
+  title:string;
   chat: Chat;
 }
 
 export interface Chat {
   id: string;
   createOn: Date;
+  messages: Message[];
+}
+
+export interface Message {
+  id: string;
+  message: string;
 }
