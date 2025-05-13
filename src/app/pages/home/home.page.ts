e50d839431c74163df3f413209810931e8a8add0
luis jair <jair.escalante@phs.mx>
2025-05-13 13:28:24 -0600

    add last details

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Chats } from 'src/app/interfaces/chats.interface';
 import { User } from 'src/app/interfaces/user.interface';
@@ -13,6 +14,7 @@ import { WebSocketService } from 'src/app/services/web-socket.service';
 export class HomePage implements OnInit {
   constructor(
     private webSocket: WebSocketService,
+    private router: Router,
   ) {}
   private jwtService = new JwtHelperService();
   private token: string = '';
@@ -37,5 +39,16 @@ export class HomePage implements OnInit {
         console.error(error);
       },
     });
+  }
+  public getTime(createdAt: string) {
+    const date = new Date(createdAt).toLocaleString();
+    const timeSplit = date.split(', ')[1];
+    const times = timeSplit.split(':');
+    const schedule = times[2].split(' ')[1];
+    return `${times[0]}:${times[1]} ${schedule}`;
+  }
+  public signOut():void{
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/login');
   }
 }
