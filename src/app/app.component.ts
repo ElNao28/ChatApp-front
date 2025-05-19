42e6a1b882aea8d2d9e30b6fcda86f240532bb53
luis jair <jair.escalante@phs.mx>
2025-05-19 13:04:42 -0600

    fix input event and add handler statubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+
+import { StatusBar } from '@capacitor/status-bar';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +11,15 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+
+  private async initializeApp() {
+    await this.platform.ready();
+
+    if (Capacitor.getPlatform() !== 'web') {
+      await StatusBar.setOverlaysWebView({ overlay: false });
+    }
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   // API_URL: 'http://localhost:3000/',
   // WS_URL: 'ws://localhost:3000/message'
-  API_URL: 'https://b705-2806-2f0-4021-de89-f1c7-6ee-adb0-a192.ngrok-free.app/',
+  API_URL:
+    'https://5398-2806-2f0-4021-de89-3904-11b5-58ed-8120.ngrok-free.app/',
   WS_URL:
-    'wss://b705-2806-2f0-4021-de89-f1c7-6ee-adb0-a192.ngrok-free.app/message',
+    'wss://5398-2806-2f0-4021-de89-3904-11b5-58ed-8120.ngrok-free.app/message',
 };
